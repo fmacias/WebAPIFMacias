1cf4ab34794d442c58d029a47effbbd980a9989c
fmaciasruano <fmaciasruano@gmail.com>
2021-05-04 21:06:15 +0200

    Refactor Renaming
    Renamins issue for better understanding, that
    the Adapter is the one that implement the
    concrete implementation to access the DataSource.

--- WebAPIFMacias/Models/PersonsRepository.cs
+++ WebAPIFMacias/Models/PersonsRepository.cs
@@ -7,24 +7,24 @@ namespace WebAPIFMacias.Models
 {
     public class PersonsRepository : IPersonsRepository
     {
-        private readonly IPersonsDataSourceAdapter _personDataSource;
-        public PersonsRepository(IPersonsDataSourceAdapter personDataSource)
+        private readonly IPersonsDataSourceAdapter _personDataSourceAdapter;
+        public PersonsRepository(IPersonsDataSourceAdapter personDataSourceAdapter)
         {
-            _personDataSource = personDataSource;
+            _personDataSourceAdapter = personDataSourceAdapter;
         }
         public IEnumerable<Person> GetAll()
         {
-            return _personDataSource.GetAll();
+            return _personDataSourceAdapter.GetAll();
         }
 
         public Person GetPerson(long id)
         {
-            return _personDataSource.GetPersonById(id);
+            return _personDataSourceAdapter.GetPersonById(id);
         }
 
         public IEnumerable<Person> GetPersonsByColor(int color)
         {
-            return _personDataSource.GetPersonsByColor(color);
+            return _personDataSourceAdapter.GetPersonsByColor(color);
         }
     }
 }
